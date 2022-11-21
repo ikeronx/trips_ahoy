c35c298031264a82d34c5383d76a93bcfaf49ee5
Keron Williams <ikeronw@gmail.com>
2022-11-21 00:15:31 -0500

    made changes

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,17 @@
 import legacy from '@vitejs/plugin-legacy';
+import topLevelAwait from 'vite-plugin-top-level-await';
 
 export default {
+        base: '/trips_ahoy/',
         plugins: [
                 legacy({
                         targets: ['defaults', 'not IE 11'],
+                }),
+                topLevelAwait({
+                        // The export name of top-level await promise for each chunk module
+                        promiseExportName: '__tla',
+                        // The function to generate import names of top-level await promise in each chunk module
+                        promiseImportName: (i) => `__tla_${i}`,
                 }),
         ],
 };
